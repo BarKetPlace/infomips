9cef44342bc50e40a722d40c73ff284eb6b80944
BarKetPlace <antoinehonor@gmail.com>
2014-10-15 13:28:16 +0200

    disp.c

---
+++ src/cmd/disasm.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include "reg.h"
+#include "cmd.h"
+#include "emulateur.h"
+#include "is_.h"
+#include "notify.h"
+#include "mem.h"
+
+int disasmcmd()
+{
+	return CMD_OK_RETURN_VALUE;
+}
