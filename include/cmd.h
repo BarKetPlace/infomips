d5c2bf3b537c603bbe6e4f2ebd4f0dac982092ed
BarKetPlace <antoinehonor@gmail.com>
2014-10-15 14:37:30 +0200

    cmd

--- include/cmd.h
+++ include/cmd.h
@@ -40,10 +40,10 @@ int exitcmd(interpreteur inter);
 int loadcmd(char* fichier, mem memory, registre* reg);
 int dispcmd(interpreteur inter, mem memory, registre* reg);
 
-int disasm(mem memory, int val, char* res);
+int disasm(mem memory, int adrr, int val, char* res);
 int disasm_(interpreteur inter, mem memory, registre* reg, int debut, int fin);
 int disasmcmd(interpreteur inter, mem memory, registre* reg);
-
+int swap_mot(int mot);
 
 
 #endif
