3e6b02b4ad0e6fc98c444b2fa983ca23368fe6ed
BarKetPlace <antoinehonor@gmail.com>
2014-10-23 16:50:47 +0200

    modif disasmx

--- src/cmd/disasm.c
+++ src/cmd/disasm.c
@@ -20,17 +20,23 @@
 int disasm(mem memory, int adrr, int val, Liste dico)
 {	//int val_swapped = swap_mot(val);
 	//printf("swap(0x%08x) = 0x%08x\n",val, val_swapped );
-	DEBUG_MSG(" 0x%08x: 0x%08x ",adrr, val);
-	printf("0x%08x: 0x%08x ",adrr, val);
+	//DEBUG_MSG(" 0x%08x: 0x%08x ",adrr, val);
+	
 	instruc mot;
 	mot.code=val;	
 	//DEBUG_MSG("");
 	definition def =NULL;
 	def = find_def(dico, mot);
 	//DEBUG_MSG("");
-	if (!def)  {ERROR_MSG(" Instruction inconnue "); return CMD_EXIT_RETURN_VALUE;}//Si def == NULL
+	if (!def)  {
+		ERROR_MSG(" Instruction inconnue ");
+		return CMD_EXIT_RETURN_VALUE;
+		}//Si def == NULL
 	//detail_def(def);
+	
+	printf("0x%08x: 0x%08x ",adrr, val);
 	print_disasm(def, mot);
+	return CMD_OK_RETURN_VALUE;
 }
 
 
@@ -76,6 +82,7 @@ int disasmcmd(interpreteur inter, mem memory, registre* reg, Liste dico)
 	char* res[MAX_STR];
 	uint32_t debut,fin,decalage;
 	token = get_next_token(inter);
+	
 	if (token && is_hexa(token)){ //disasm HEXA
 		sscanf(token, "%x", &debut); //DEBUG_MSG(" sfsg ");
 		if (debut%4 != 0) debut = debut - (debut%4);
@@ -120,7 +127,7 @@ int disasmcmd(interpreteur inter, mem memory, registre* reg, Liste dico)
 		}
 		else { //DEBUG_MSG("");
 			//printf("0x%08x",debut);
-			if (!disasm(memory,debut , find_val(memory, debut), dico)) return CMD_EXIT_RETURN_VALUE;
+			if ( disasm(memory,debut , find_val(memory, debut), dico) != CMD_OK_RETURN_VALUE) return CMD_EXIT_RETURN_VALUE;
 			
 			return CMD_OK_RETURN_VALUE ;
 		}
