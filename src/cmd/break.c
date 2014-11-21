1902d87342f6a552455051ee535e4fb18e17a05c
BarKetPlace <antoinehonor@gmail.com>
2014-11-21 08:54:30 +0100

    amelioration break

--- src/cmd/break.c
+++ src/cmd/break.c
@@ -1,26 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "cmd.h"
 #include "dico.h"
 #include "is_.h"
-int breakcmd(interpreteur inter,mem memory, Liste_int breaklist)
+
+int breakcmd(interpreteur inter,mem memory, Liste_int* pbreaklist)
 {
 	char* token;
 	token = get_next_token(inter);
 	if (token && !strcmp(token, "add")){
-		return break_addcmd(inter,memory, breaklist);
+		return break_addcmd(inter,memory, pbreaklist);
 	}
 	else if (token && !strcmp(token, "del")){
-		return break_delcmd(inter, breaklist);
+		return break_delcmd(inter, pbreaklist);
 	}
 	else if (token && !strcmp(token, "list")){
-		if (breaklist) printf("%d\n",breaklist->val);
+		if (*pbreaklist) printf("%d\n",(*pbreaklist)->val);
 		else DEBUG_MSG("null");
-		return break_listcmd(breaklist);
+		return break_listcmd(*pbreaklist);
 	}
-	else  WARNING_MSG("Syntax error break (add ou del) + HEXAs | list");
-	
+	else {
+		WARNING_MSG("Syntax error break (add ou del) + HEXAs | list");
+		return cmd_unknown;
+	}
+
 }
 
-int break_delcmd(interpreteur inter, Liste_int breaklist){
+int break_delcmd(interpreteur inter, Liste_int* pbreaklist){
 	return cmd_ok;
 }
 
@@ -29,7 +36,7 @@ int break_listcmd(Liste_int breaklist){
 	//visualiser_int(breaklist);
 	return cmd_ok;
 }
-int break_addcmd(interpreteur inter, mem memory , Liste_int breaklist)
+int break_addcmd(interpreteur inter, mem memory , Liste_int* pbreaklist)
 { 
 	char* token;
 	uint32_t adresse;
@@ -44,11 +51,11 @@ int break_addcmd(interpreteur inter, mem memory , Liste_int breaklist)
 			sscanf(token,"%x", &adresse);
 			DEBUG_MSG("%x",adresse);
 			if (is_text_adresse(memory,adresse,&text_start,&text_taille) == cmd_ok){
-				//DEBUG_MSG("");
-				breaklist = ajout_tete_int(adresse, breaklist);
+				DEBUG_MSG("");
+				*pbreaklist = ajout_tete_int(adresse, *pbreaklist); DEBUG_MSG("");
 			}
 			else{
-				WARNING_MSG("0x%08x n'est pas dans la zone texte (%x->%x)", adresse,text_start,text_start + text_taille);
+				WARNING_MSG("0x%08x n'est pas dans la zone texte (%x->%x)", adresse,text_start,text_start + text_taille-1);
 				return cmd_unknown;
 			}
 		}
@@ -59,15 +66,17 @@ int break_addcmd(interpreteur inter, mem memory , Liste_int breaklist)
 		
 		token = get_next_token(inter);	
 	}
-	printf("%d\n",breaklist->val);
+	//DEBUG_MSG("");
+	DEBUG_MSG("%x",(*pbreaklist)->val);
 	return cmd_ok;
 }
 
 //fonction agissant sur des listes de nombre
 Liste_int ajout_tete_int(uint32_t c, Liste_int l)
-{	Liste_int p =(Liste_int) calloc(1, sizeof(*p));
+{	DEBUG_MSG("");
+	Liste_int p =(Liste_int) calloc(1, sizeof(*p)); DEBUG_MSG("");
 	p->val=c;	
-	p->suiv=l;DEBUG_MSG("%x",c);
+	p->suiv=l; DEBUG_MSG("%x",c);
 	return p;
 }
 
