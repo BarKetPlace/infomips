eb64e5b0d3bf12d5c14c2373eaa8e2567b1f87f7
Maxence PICHAT <spichat@MacBook-Pro-de-Serge.local>
2014-11-21 11:46:07 +0100

    modifs reg.c (enlever le reg v2)@ e Please enter the commit message for your changes. Lines starting

--- src/fct/add.c
+++ src/fct/add.c
@@ -22,9 +22,15 @@ int fct_add(inst k, mem memory, registre* regs)
 
 int main()
 {
-	
-	long long temp=regs[k.r.rs].val+regs[k.r.rt].val;
-	if (temp<=max_inst) {regs[k.r.rd].val=temp; return cmd_ok;}
-	else {return cmd_unknown;}
+	inst l;
+	int l.r.rs;
+	int l.r.rt;
+	scanf("l.r.rs = %d",&(l.r.rs));
+	scanf("l.r.rt = %d",&(l.r.rt));
+	mem memory;
+	registre* reg;
+	int u=fct_add(l,memory,reg);
+	printf("rd = 0x08%x", reg[k.r.rd].val);
+	return 1;
 }
 
