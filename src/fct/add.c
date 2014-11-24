dbe68ea74982428834b318d7949967592cdc39fc
Maxence PICHAT <spichat@MacBook-Pro-de-Serge.local>
2014-11-24 17:31:25 +0100

    ajout de add.c

---
+++ src/fct/add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <strings.h>
+#include <string.h>
+
+#include "mem.h"
+#include "reg.h"
+#include "bits.h"
+#include "notify.h"
+#include "fct.h"
+#include "dico.h"
+
+#include "typesmem.h"
+
+
+// Fonction add
+// Definir max_inst
+
+
+int fct_add(inst k, mem memory, registre* reg, Liste dico)
+
+{
+	 
+	long long temp=reg[k.r.rs].val+reg[k.r.rt].val;
+	if (temp<=max_inst) {reg[k.r.rd].val=temp; return cmd_ok;}
+	else {WARNING_MSG("Erreur"); return cmd_unknown;}
+}
+
+
+/*int main()
+{
+	inst l;
+	Liste dico=NULL;
+	l.r.rs=2;
+	l.r.rt=6;
+	//scanf("l.r.rs = %d",&(l.r.rs));
+	//scanf("l.r.rt = %d",&(l.r.rt));
+	mem memory;
+	registre* reg;
+	int u=fct_add(l,memory,reg, dico);
+	printf("rd = 0x%08x", reg[l.r.rd].val);
+	return 1;
+}*/
+
